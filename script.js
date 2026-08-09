7948315835920b33f28e5433e6c8b7c90ab6dab9
mahema0813 <mahema0813@gmail.com>
2026-08-09 11:16:37 +0530

    Add bio button functionality to display bio text

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+let bioButton = document.getElementById("bioButton");
+let bioText = document.getElementById("bio")
+bioButton.addEventListener("Click", function() {
+  bioText.textContent = "I've been coding for 2 years, love clean design, and I'm currently learning JavaScript to build more interactive projects?!";
+});
