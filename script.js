1d035f905a7f2b71d6ff109885be9da28a0ce8c3
mahema0813 <mahema0813@gmail.com>
2026-08-09 16:57:42 +0530

    Add color variables for pink and red

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ let bioText = document.getElementById("bio")
 bioButton.addEventListener("Click", function() {
   bioText.textContent = "I've been coding for 2 years, love clean design, and I'm currently learning JavaScript to build more interactive projects?!";
 });
+let pink = "#FFC0CB";
+let red = "#FF0000";
